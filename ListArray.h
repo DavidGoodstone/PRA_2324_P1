1c79a402bc54859979689fad9a5ba7ecc32ad800
DPIEGAR <dpiegar@alumno.upv.es@linux-vdi02.upvnet.upv.es>
2023-10-09 13:11:36 +0200

    Añadido Makefile y código de test de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,17 +1,19 @@
-#include <iostream>
+#include <ostream>
 #include "List.h"
 
-class ListArray{
+template <typename T>
+
+class ListArray : public List<T>{
 	private:
 		T* arr;
 		int max;
 		int n;
-		static const int MINSIZE;ListArray();
+		static const int MINSIZE;//ListArray();
 		void resize(int new_size);
 		
 	public:
 		ListArray();
 		~ListArray();
 		T operator[](int pos);
-		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
+		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &List);
 };
