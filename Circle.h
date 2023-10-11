df282ecd023677b669b7c506f9c8b865589b8e37
DPIEGAR <dpiegar@alumno.upv.es@linux-vdi02.upvnet.upv.es>
2023-10-11 16:24:55 +0200

    Añadido Makefile y código de test de la clase Circle

---
+++ Circle.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Shape.h"
+
+class Circle : public Shape {
+    private:
+    	Point2D center;
+    	double radius;
+    public:
+    	Circle();
+    	Circle(std::string color, Point2D center, double radius);
+    	Point2D get_center() const;
+    	void set_center(Point2D p);
+    	double get_radius() const;
+    	void set_radius(double r);
+    	std::ostream& operator<<(std::ostream &out, const Circle &c);
+};
